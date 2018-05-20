8de1f041109ecf9285a969a638ca1710e199b885
JmCg <jmc-g@hotmail.com>
2018-05-20 09:16:41 -0300

    cambios 20-05-2018

--- Javascript/codigo.js
+++ Javascript/codigo.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
-
-  /*$('a#quienessomos').click(function(e) {
+    
+    /*$('a#quienessomos').click(function(e) {
     e.preventDefault();
     var quienessomos = $(this).attr('href');
     
@@ -9,48 +9,12 @@ $(document).ready(function () {
 
       scrollTop:$(quienessomos).offset().top
 
-    }, 1500);
+    }, 2000);
 
   });*/
 
+    
   
-  /*$("a").on('click', function(event) {
-
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
-
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });*/
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -60,53 +24,50 @@ $(document).ready(function () {
 
   
 
-              //scrollpsy
-              $('body').scrollspy({ target: '#navbarNavDropdown' })
-                
-                
-                $(window).scroll(function(){
-                        if ($(this).scrollTop() > 100) {
-                            $('.scrollup').fadeIn();
-                        } else {
-                            $('.scrollup').fadeOut();
-                        }
-                    });
-              
-                    $('.scrollup').click(function(){
-                        $("html, body").animate({ scrollTop: 0 }, 600);
-                        return false;
-                  });
-
-
-              // Add smooth scrolling to all links in navbar + footer link
-
-
-
-
-
-
-  $("#enviadatos").click(function () {
-		var mail = $("#correo").val();
-		var mensaje = $("#mensajecliente").val();  
+    //scrollpsy
+    $('body').scrollspy({ target: '#navbarNavDropdown' })
+      
+      
+      $(window).scroll(function(){
+              if ($(this).scrollTop() > 100) {
+                  $('.scrollup').fadeIn();
+              } else {
+                  $('.scrollup').fadeOut();
+              }
+          });
     
+          $('.scrollup').click(function(){
+              $("html, body").animate({ scrollTop: 0 }, 600);
+              return false;
+        });
+
+
     
-    if (($("#correo").val().length == 0)) {
-			$("#validadorcorreo").fadeIn("slow");
-			return false;
-		}
-            else {$("#validadorcorreo").fadeOut("slow");	
-		         }
-    
-    
-    if (($("#mensajecliente").val().length == 0)) {
-			$("#validadormensaje").fadeIn("slow");
-			return false;
-		}
-            else {$("#validadormensaje").fadeOut("slow");	
-		         }
-    
-    
-});
+
+          //Validacion datos de formulario
+
+          $("#enviadatos").click(function () {
+            var mail = $("#correo").val();
+            var mensaje = $("#mensajecliente").val();  
+            
+            
+            if (($("#correo").val().length == 0)) {
+              $("#validadorcorreo").fadeIn("slow");
+              return false;
+            }
+                    else {$("#validadorcorreo").fadeOut("slow");	
+                    }
+            
+            
+            if (($("#mensajecliente").val().length == 0)) {
+              $("#validadormensaje").fadeIn("slow");
+              return false;
+            }
+                    else {$("#validadormensaje").fadeOut("slow");	
+                    }
+            
+            
+        });
 
 
 
